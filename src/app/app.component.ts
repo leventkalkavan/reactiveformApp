704ba53470db166082ee30965123d56567e1d9c4
leventkalkavan <levent.kalkavan@hotmail.com>
2023-07-07 18:42:12 +0300

    firstCommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,71 @@
-import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'reactiveformApp';
+export class AppComponent implements OnInit {
+  constructor(private _date: DatePipe) {}
+
+  ngOnInit(): void {
+    this.createAddForm();
+    this.createUpdateForm();
+  }
+
+  addForm: FormGroup = new FormGroup({});
+  updateForm: FormGroup = new FormGroup({});
+  isUpdateFormActive: boolean = false;
+  updateIndex: number = 0;
+  employee: Employee[] = [];
+
+  createAddForm() {
+    this.addForm = new FormGroup({
+      name: new FormControl("", [Validators.required, Validators.minLength(3)]),
+      startDate: new FormControl(this._date.transform(new Date(), "yyyy-MM-dd")),
+      proffesion: new FormControl("", [Validators.required, Validators.minLength(3)])
+    });
+  }
+
+  save() {
+    if (this.addForm.valid) {
+      this.employee.push(this.addForm.value);
+      this.addForm.reset();
+    }
+  }
+
+  createUpdateForm() {
+    this.updateForm = new FormGroup({
+      name: new FormControl("", [Validators.required, Validators.minLength(3)]),
+      proffesion: new FormControl("", [Validators.required, Validators.minLength(3)])
+    });
+  }
+
+  get(item: any, index: number) {
+    this.addForm.patchValue({
+      name: item.name,
+      proffesion: item.proffesion,
+      startDate: item.startDate
+    });
+    this.isUpdateFormActive = true;
+    this.updateIndex = index;
+  }
+
+  cancel() {
+    this.isUpdateFormActive = false;
+  }
+
+  update() {
+    if (this.updateForm.valid) {
+      this.employee[this.updateIndex] = this.updateForm.value;
+      this.cancel();
+    }
+  }
+}
+
+class Employee {
+  name: string = "";
+  startDate: string = "";
+  proffesion: string = "";
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { DatePipe } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -10,9 +11,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [DatePipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
